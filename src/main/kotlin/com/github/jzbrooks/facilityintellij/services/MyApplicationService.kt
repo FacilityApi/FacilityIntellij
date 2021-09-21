f30f70bec092e0f9ea142df7ec63c73f75912165
GitHub Action <action@github.com>
2021-09-21 02:20:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/jzbrooks/facilityintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jzbrooks/facilityintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jzbrooks.facilityintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jzbrooks.facilityintellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jzbrooks/facilityintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jzbrooks/facilityintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jzbrooks.facilityintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jzbrooks.facilityintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
