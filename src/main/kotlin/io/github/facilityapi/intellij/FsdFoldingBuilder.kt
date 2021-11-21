ca770600fb1430f0b10cf41a94a85808163f0a3c
Justin Brooks <justinbrooks@fastmail.com>
2021-11-21 00:10:51 -0600

    Add code folding

---
+++ src/main/kotlin/io/github/facilityapi/intellij/FsdFoldingBuilder.kt
@@ -0,0 +1,56 @@
+package io.github.facilityapi.intellij
+
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.FoldingGroup
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.refactoring.suggested.endOffset
+import io.github.facilityapi.intellij.psi.*
+
+class FsdFoldingBuilder : FoldingBuilderEx() {
+    override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+        return FOLDABLES.flatMap { foldable ->
+            PsiTreeUtil.findChildrenOfType(root, foldable).mapNotNull { child ->
+                when (child) {
+                    is FsdServiceSpec -> {
+                        FoldingDescriptor(
+                            child.node,
+                            TextRange(child.serviceItems.textRange.startOffset + 1, child.serviceItems.textRange.endOffset - 1),
+                            FoldingGroup.newGroup("Fsd Service Folding Group ${child.identifier}")
+                        )
+                    }
+
+                    is FsdMethodSpec,
+                    is FsdDataSpec,
+                    is FsdEnumSpec,
+                    is FsdErrorSetSpec -> {
+                        val start = PsiTreeUtil.findSiblingForward(child.firstChild, FsdTypes.LEFT_BRACE, null) ?: return@mapNotNull null
+                        val range = TextRange(start.textOffset + 1, child.endOffset - 1)
+                        val group = FoldingGroup.newGroup("Fsd Service Item Body Group")
+                        FoldingDescriptor(child.node, range, group)
+                    }
+
+                    else -> null
+                }
+            }
+        }.toTypedArray()
+    }
+
+    override fun getPlaceholderText(node: ASTNode): String = "..."
+
+    override fun isCollapsedByDefault(node: ASTNode): Boolean = false
+
+    companion object {
+        val FOLDABLES = setOf(
+            FsdServiceSpec::class.java,
+            FsdMethodSpec::class.java,
+            FsdDataSpec::class.java,
+            FsdEnumSpec::class.java,
+            FsdErrorSetSpec::class.java,
+        )
+    }
+}
