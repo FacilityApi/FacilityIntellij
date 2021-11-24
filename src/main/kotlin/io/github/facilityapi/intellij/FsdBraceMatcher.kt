e9495b97a5c3c09fbc60c82c3baee5b977ab73a7
Justin Brooks <justin@jzbrooks.com>
2021-11-23 22:53:22 -0600

    Add a brace matcher

---
+++ src/main/kotlin/io/github/facilityapi/intellij/FsdBraceMatcher.kt
@@ -0,0 +1,19 @@
+package io.github.facilityapi.intellij
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import io.github.facilityapi.intellij.psi.FsdTypes
+
+class FsdBraceMatcher : PairedBraceMatcher {
+    override fun getPairs() = arrayOf(
+        BracePair(FsdTypes.LEFT_BRACE, FsdTypes.RIGHT_BRACE, true),
+        BracePair(FsdTypes.LEFT_BRACKET, FsdTypes.RIGHT_BRACKET, false),
+        BracePair(FsdTypes.LEFT_ANGLE, FsdTypes.RIGHT_ANGLE, false),
+    )
+
+    override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?) = true
+
+    override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int) = openingBraceOffset
+}
