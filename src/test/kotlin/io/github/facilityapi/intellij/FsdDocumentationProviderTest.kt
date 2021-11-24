bfd1601a875128bf9ca5c058f6a9b5c8e852e86d
Justin Brooks <justin@jzbrooks.com>
2021-11-23 21:56:55 -0600

    Add a test

---
+++ src/test/kotlin/io/github/facilityapi/intellij/FsdDocumentationProviderTest.kt
@@ -0,0 +1,53 @@
+package io.github.facilityapi.intellij
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import com.intellij.codeInsight.documentation.DocumentationManager
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class FsdDocumentationProviderTest : BasePlatformTestCase() {
+    fun `test documentation`() {
+        myFixture.configureByText(
+            FsdFileType,
+            """
+                service FsdFindUsages
+                {
+                    data Car
+                    {
+                        wheel: Wheel[];
+                        spare: Whe<caret>el;
+                    }
+
+                    /// It's circular
+                    data Wheel
+                    {
+                        id: int64;
+                    }
+                }
+            """.trimIndent()
+        )
+        val originalElement = myFixture.elementAtCaret
+        var element = DocumentationManager
+            .getInstance(project)
+            .findTargetElement(myFixture.editor, originalElement.containingFile, originalElement)
+
+        if (element == null) {
+            element = originalElement
+        }
+
+        val documentationProvider = DocumentationManager.getProviderFromElement(element);
+        val generateDoc = documentationProvider.generateDoc(element, originalElement);
+
+        assertThat(generateDoc, "generatedDoc").isNotNull().isEqualTo(
+            """
+                <div class='definition'><pre>data Wheel</pre></div>
+                <div class='content'> It's circular</div>
+                <table class='sections'>
+                <tr><td valign='top' class='section'>
+                <p>File:</td><td valign='top'><p>aaa.fsd</td>
+                </table>
+            """.trimIndent().replace("\n", "")
+        )
+    }
+}
