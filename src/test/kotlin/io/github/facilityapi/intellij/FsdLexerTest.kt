2d132684276395119e5d1347e171e167dbfa19d3
Justin Brooks <justin@jzbrooks.com>
2021-12-05 19:08:18 -0600

    Add tests

--- src/test/kotlin/io/github/facilityapi/intellij/FsdLexerTest.kt
+++ src/test/kotlin/io/github/facilityapi/intellij/FsdLexerTest.kt
@@ -8,6 +8,36 @@ class FsdLexerTest : LexerTestCase() {
     override fun createLexer(): Lexer = FsdLexerAdapter()
     override fun getDirPath(): String = "src/test/testData"
 
+    fun `test lexer`() {
+        doTest(
+            """
+                [CSharp(namespace: test)]
+                service WidgetAPI
+                {
+                }
+            """.trimIndent(),
+            """
+                [ ('[')
+                attributename ('CSharp')
+                ( ('(')
+                identifier ('namespace')
+                : (':')
+                WHITE_SPACE (' ')
+                attributeparametervalue ('test')
+                ) (')')
+                ] (']')
+                WHITE_SPACE ('\n')
+                service ('service')
+                WHITE_SPACE (' ')
+                identifier ('WidgetAPI')
+                WHITE_SPACE ('\n')
+                { ('{')
+                WHITE_SPACE ('\n')
+                } ('}')
+            """.trimIndent()
+        )
+    }
+
     fun `test correct restart`() {
         checkCorrectRestartOnEveryToken(
             """
@@ -15,6 +45,14 @@ class FsdLexerTest : LexerTestCase() {
                 service WidgetAPI
                 {
                 }
+            """.trimIndent()
+        )
+    }
+
+    fun `test correct restart on invalid token`() {
+        checkCorrectRestartOnEveryToken(
+            """
+                servi
             """.trimIndent()
         )
     }
