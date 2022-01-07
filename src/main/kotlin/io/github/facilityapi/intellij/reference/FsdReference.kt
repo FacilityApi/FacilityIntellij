fbd11eca5ea1d429db3276709f6280ce7a338d89
Justin Brooks <justin@jzbrooks.com>
2022-01-06 20:12:24 -0600

    Add declaration type icons in autocomplete UI
    Fixes #19

--- src/main/kotlin/io/github/facilityapi/intellij/reference/FsdReference.kt
+++ src/main/kotlin/io/github/facilityapi/intellij/reference/FsdReference.kt
@@ -1,12 +1,17 @@
 package io.github.facilityapi.intellij.reference
 
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.PsiReferenceBase
-import io.github.facilityapi.intellij.FsdFileType
 import io.github.facilityapi.intellij.findTypeDefinitions
+import io.github.facilityapi.intellij.psi.FsdDataSpec
+import io.github.facilityapi.intellij.psi.FsdEnumSpec
+import io.github.facilityapi.intellij.psi.FsdErrorSetSpec
+import io.github.facilityapi.intellij.psi.FsdMethodSpec
+import io.github.facilityapi.intellij.psi.FsdServiceSpec
 
 class FsdReference(element: PsiElement, textRange: TextRange) : PsiReferenceBase<PsiElement>(element, textRange) {
 
@@ -21,10 +26,16 @@ class FsdReference(element: PsiElement, textRange: TextRange) : PsiReferenceBase
 
     override fun getVariants(): Array<Any> {
         return findTypeDefinitions(myElement.project).map { type ->
-            LookupElementBuilder
-                .create(type)
-                .withIcon(FsdFileType.icon)
-                .withTypeText(type.containingFile.name)
+            val icon = when (type.parent) {
+                is FsdDataSpec -> IconLoader.getIcon("/icons/data.svg", FsdDataSpec::class.java)
+                is FsdEnumSpec -> IconLoader.getIcon("/icons/enum.svg", FsdEnumSpec::class.java)
+                is FsdErrorSetSpec -> IconLoader.getIcon("/icons/error-set.svg", FsdErrorSetSpec::class.java)
+                is FsdMethodSpec -> IconLoader.getIcon("/icons/method.svg", FsdMethodSpec::class.java)
+                is FsdServiceSpec -> IconLoader.getIcon("/icons/service.svg", FsdServiceSpec::class.java)
+                else -> null
+            }
+
+            LookupElementBuilder.create(type).withIcon(icon)
         }.toList().toTypedArray()
     }
 
