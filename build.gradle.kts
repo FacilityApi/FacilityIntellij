1b4cd46a8a52decd0b6e1c7bf94d3834d4fd0356
Justin Brooks <justin@jzbrooks.com>
2022-08-04 20:23:50 -0500

    Bump the target platform version

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk"))
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
 }
 
