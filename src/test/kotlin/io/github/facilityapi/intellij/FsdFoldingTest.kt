48fd959edbf394496bfaecd618a4a2571439260a
Justin Brooks <justin.brooks@faithlife.com>
2022-08-09 16:39:12 -0500

    Upgrade tools

--- src/test/kotlin/io/github/facilityapi/intellij/FsdFoldingTest.kt
+++ src/test/kotlin/io/github/facilityapi/intellij/FsdFoldingTest.kt
@@ -6,6 +6,7 @@ class FsdFoldingTest : BasePlatformTestCase() {
     override fun getTestDataPath(): String = "src/test/testData"
 
     fun `test successful folding`() {
+        myFixture.configureByFile("folding/folding.fsd")
         myFixture.testFolding("$testDataPath/folding/folding.fsd")
     }
 }
