f90192e93d2ed4ed656ec7ad4ef8b92c6f6c0f01
Justin Brooks <justin@jzbrooks.com>
2022-08-16 16:50:54 -0500

    Add service members in duplicate item check

--- src/main/kotlin/io/github/facilityapi/intellij/inspection/DuplicateMemberInspection.kt
+++ src/main/kotlin/io/github/facilityapi/intellij/inspection/DuplicateMemberInspection.kt
@@ -13,27 +13,47 @@ import io.github.facilityapi.intellij.psi.FsdDataSpec
 import io.github.facilityapi.intellij.psi.FsdDecoratedField
 import io.github.facilityapi.intellij.psi.FsdEnumSpec
 import io.github.facilityapi.intellij.psi.FsdErrorList
+import io.github.facilityapi.intellij.psi.FsdErrorSetSpec
 import io.github.facilityapi.intellij.psi.FsdMethodSpec
+import io.github.facilityapi.intellij.psi.FsdServiceItems
 
-class DuplicateNameInspection : LocalInspectionTool() {
+class DuplicateMemberInspection : LocalInspectionTool() {
     override fun buildVisitor(
         holder: ProblemsHolder,
         isOnTheFly: Boolean,
         session: LocalInspectionToolSession
     ): PsiElementVisitor = object : PsiElementVisitor() {
-        private val fix = Fix()
+        private val serviceMemberFix = ServiceFix()
+        private val fieldFix = FieldFix()
 
         override fun visitElement(element: PsiElement) {
+            if (element is FsdServiceItems) {
+                val seenItems = hashSetOf<String>()
+                for (item in element.decoratedServiceItemList) {
+                    val serviceItemIdentifier = item.dataSpec?.identifierDeclaration ?:
+                    item.methodSpec?.identifierDeclaration ?:
+                    item.errorSetSpec?.identifierDeclaration ?:
+                    item.enumSpec?.identifierDeclaration
+
+                    val name = serviceItemIdentifier?.identifier?.text
+
+                    if (name != null && !seenItems.add(name)) {
+                        val message = FsdBundle.getMessage("inspections.bugs.duplicate.member.service", name)
+                        holder.registerProblem(serviceItemIdentifier, message, serviceMemberFix)
+                    }
+                }
+            }
+
             if (element is FsdMethodSpec) {
                 val requestFields = element.requestFields?.decoratedFieldList ?: emptyList()
-                checkForFieldDuplicates(requestFields, "inspections.bugs.duplicatename.requestfield")
+                checkForFieldDuplicates(requestFields, "inspections.bugs.duplicate.member.requestfield")
 
                 val responseFields = element.responseFields?.decoratedFieldList ?: emptyList()
-                checkForFieldDuplicates(responseFields, "inspections.bugs.duplicatename.responsefield")
+                checkForFieldDuplicates(responseFields, "inspections.bugs.duplicate.member.responsefield")
             }
 
             if (element is FsdDataSpec) {
-                checkForFieldDuplicates(element.decoratedFieldList, "inspections.bugs.duplicatename.field")
+                checkForFieldDuplicates(element.decoratedFieldList, "inspections.bugs.duplicate.member.field")
             }
 
             if (element is FsdEnumSpec) {
@@ -41,8 +61,8 @@ class DuplicateNameInspection : LocalInspectionTool() {
                 for (case in element.enumValueList?.decoratedEnumValueList ?: emptyList()) {
                     val caseName = case.enumValue!!.identifier.text
                     if (!seenCases.add(caseName)) {
-                        val message = FsdBundle.getMessage("inspections.bugs.duplicatename.enumerated", caseName)
-                        holder.registerProblem(case, message, fix)
+                        val message = FsdBundle.getMessage("inspections.bugs.duplicate.member.enumerated", caseName)
+                        holder.registerProblem(case, message, fieldFix)
                     }
                 }
             }
@@ -52,8 +72,8 @@ class DuplicateNameInspection : LocalInspectionTool() {
                 for (decoratedError in element.decoratedErrorSpecList) {
                     val errorName = decoratedError.errorSpec!!.identifier.text
                     if (!seenFields.add(errorName)) {
-                        val message = FsdBundle.getMessage("inspections.bugs.duplicatename.error", errorName)
-                        holder.registerProblem(decoratedError, message, fix)
+                        val message = FsdBundle.getMessage("inspections.bugs.duplicate.member.error", errorName)
+                        holder.registerProblem(decoratedError, message, fieldFix)
                     }
                 }
             }
@@ -66,13 +86,13 @@ class DuplicateNameInspection : LocalInspectionTool() {
                 val fieldName = decoratedField.field.identifier.text
                 if (!seenFields.add(fieldName)) {
                     val message = FsdBundle.getMessage(bundleKey, fieldName)
-                    holder.registerProblem(decoratedField, message, fix)
+                    holder.registerProblem(decoratedField, message, fieldFix)
                 }
             }
         }
     }
 
-    class Fix : LocalQuickFix {
+    class FieldFix : LocalQuickFix {
         override fun getFamilyName() = NAME
 
         override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
@@ -80,7 +100,24 @@ class DuplicateNameInspection : LocalInspectionTool() {
         }
 
         companion object {
-            val NAME = FsdBundle.getMessage("inspections.bugs.duplicatenames.quickfix")
+            val NAME = FsdBundle.getMessage("inspections.bugs.duplicate.member.quickfix")
+        }
+    }
+
+    class ServiceFix : LocalQuickFix {
+        private val PsiElement.isServiceItem: Boolean
+            get() = this is FsdMethodSpec || this is FsdDataSpec || this is FsdEnumSpec || this is FsdErrorSetSpec
+
+        override fun getFamilyName() = NAME
+
+        override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+            var element: PsiElement? = descriptor.psiElement
+            while (element != null && !element.isServiceItem) { element = element.parent }
+            element?.delete()
+        }
+
+        companion object {
+            val NAME = FsdBundle.getMessage("inspections.bugs.duplicate.member.quickfix")
         }
     }
 }
