93a0c7a00a57a4665c13840dd2abb6572de2eebf
Justin Brooks <justin@jzbrooks.com>
2022-08-20 17:15:50 -0500

    Implement the inspection

--- src/main/kotlin/io/github/facilityapi/intellij/inspection/UnusedTypeInspection.kt
+++ src/main/kotlin/io/github/facilityapi/intellij/inspection/UnusedTypeInspection.kt
@@ -1,32 +1,29 @@
 package io.github.facilityapi.intellij.inspection
 
-import com.intellij.codeInsight.daemon.impl.HighlightInfoType
 import com.intellij.codeInspection.LocalInspectionTool
-import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
-import com.intellij.psi.util.PsiTreeUtil
-import io.github.facilityapi.intellij.FsdBundle
-import io.github.facilityapi.intellij.isCollection
-import io.github.facilityapi.intellij.isEnum
-import io.github.facilityapi.intellij.isNumber
-import io.github.facilityapi.intellij.psi.FsdAttribute
-import io.github.facilityapi.intellij.psi.FsdAttributeParameter
-import io.github.facilityapi.intellij.psi.FsdDecoratedElement
-import io.github.facilityapi.intellij.psi.FsdDecoratedEnumValue
-import io.github.facilityapi.intellij.psi.FsdDecoratedErrorSpec
-import io.github.facilityapi.intellij.psi.FsdDecoratedField
-import io.github.facilityapi.intellij.psi.FsdDecoratedServiceItem
-import io.github.facilityapi.intellij.psi.FsdField
-import io.github.facilityapi.intellij.supportsValidate
+import com.intellij.psi.util.descendants
+import io.github.facilityapi.intellij.psi.FsdDataSpec
+import io.github.facilityapi.intellij.psi.FsdEnumSpec
+import io.github.facilityapi.intellij.psi.FsdNamedElement
+import io.github.facilityapi.intellij.psi.FsdReferenceType
 
 class UnusedTypeInspection : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return object : PsiElementVisitor() {
             override fun visitElement(element: PsiElement) {
-            HighlightInfoType.UNUSED_SYMBOL
+                val namedElement = element as? FsdNamedElement ?: return
+
+                val isUsed = element.containingFile.descendants()
+                    .filterIsInstance<FsdReferenceType>()
+                    .any { it.reference.isReferenceTo(namedElement) }
+
+                if (!isUsed && (namedElement.parent is FsdDataSpec || namedElement.parent is FsdEnumSpec)) {
+                    holder.registerProblem(namedElement.nameIdentifier!!, "Unused type", ProblemHighlightType.LIKE_UNUSED_SYMBOL)
+                }
             }
         }
     }
