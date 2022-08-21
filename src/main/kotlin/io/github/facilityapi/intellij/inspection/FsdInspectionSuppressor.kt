1084c141417d91b6d5f79431f36e113a74175e8c
Justin Brooks <justin@jzbrooks.com>
2022-08-21 15:17:05 -0500

    Add a slightly better implementation

--- src/main/kotlin/io/github/facilityapi/intellij/inspection/FsdInspectionSuppressor.kt
+++ src/main/kotlin/io/github/facilityapi/intellij/inspection/FsdInspectionSuppressor.kt
@@ -6,13 +6,16 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.elementType
 import com.intellij.psi.util.siblings
-import io.github.facilityapi.intellij.psi.FsdDecoratedServiceItem
+import io.github.facilityapi.intellij.psi.FsdDecoratedElement
 import io.github.facilityapi.intellij.psi.FsdTypes
 
 class FsdInspectionSuppressor : InspectionSuppressor {
     override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
-        val serviceItem = PsiTreeUtil.getParentOfType(element, FsdDecoratedServiceItem::class.java) ?: return false
-        return serviceItem.siblings(false).filter { it.elementType == FsdTypes.COMMENT }.any { it.text.contains("suppress") }
+        val serviceItem = PsiTreeUtil.getParentOfType(element, FsdDecoratedElement::class.java) ?: return false
+
+        return serviceItem.siblings(false)
+            .filter { it.elementType == FsdTypes.COMMENT }
+            .any { it.text.contains("suppress\\s+$toolId".toRegex()) }
     }
 
     override fun getSuppressActions(element: PsiElement?, toolId: String): Array<SuppressQuickFix> {
