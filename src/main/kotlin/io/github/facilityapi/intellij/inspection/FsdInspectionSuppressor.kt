4e2551bf2a3c536344929fadbc184383e77d3bc5
Justin Brooks <justin@jzbrooks.com>
2022-08-21 15:11:38 -0500

    Add a really simple implementation - WIP

--- src/main/kotlin/io/github/facilityapi/intellij/inspection/FsdInspectionSuppressor.kt
+++ src/main/kotlin/io/github/facilityapi/intellij/inspection/FsdInspectionSuppressor.kt
@@ -3,13 +3,19 @@ package io.github.facilityapi.intellij.inspection
 import com.intellij.codeInspection.InspectionSuppressor
 import com.intellij.codeInspection.SuppressQuickFix
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.elementType
+import com.intellij.psi.util.siblings
+import io.github.facilityapi.intellij.psi.FsdDecoratedServiceItem
+import io.github.facilityapi.intellij.psi.FsdTypes
 
 class FsdInspectionSuppressor : InspectionSuppressor {
     override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
-        TODO("Not yet implemented")
+        val serviceItem = PsiTreeUtil.getParentOfType(element, FsdDecoratedServiceItem::class.java) ?: return false
+        return serviceItem.siblings(false).filter { it.elementType == FsdTypes.COMMENT }.any { it.text.contains("suppress") }
     }
 
     override fun getSuppressActions(element: PsiElement?, toolId: String): Array<SuppressQuickFix> {
-        TODO("Not yet implemented")
+        return emptyArray()
     }
 }
