d8e80858cbc1285a087f42e8b7b7707ecdecf8f1
Dave Dunkin <dave.dunkin@logos.com>
2023-09-07 10:23:10 -0700

    Update live template API.

--- src/main/kotlin/io/github/facilityapi/intellij/tools/FsdLiveTemplateContext.kt
+++ src/main/kotlin/io/github/facilityapi/intellij/tools/FsdLiveTemplateContext.kt
@@ -5,7 +5,7 @@ import com.intellij.codeInsight.template.TemplateContextType
 import io.github.facilityapi.intellij.FsdLanguage
 import io.github.facilityapi.intellij.psi.FsdFile
 
-class FsdLiveTemplateContext : TemplateContextType(FsdLanguage.id, FsdLanguage.displayName) {
+class FsdLiveTemplateContext : TemplateContextType(FsdLanguage.id) {
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
         return templateActionContext.file is FsdFile
     }
